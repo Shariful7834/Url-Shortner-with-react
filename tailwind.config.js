e8f9053d585ddad537c1346f7845d5ac122847bb
Shariful Islam <engr.shariful.cse@gmail.com>
2022-12-24 19:32:32 +0100

    added the navbar

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "hsl(257, 27%, 26%)",
+
+          secondary: "hsl(0, 87%, 67%)",
+
+          accent: "#37CDBE",
+
+          neutral: "hsl(0, 0%, 75%)",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
